03551e2cea50bd2f13b470f7ca55a2a45126440a
José Huertas <jemanuel0123@hotmail.com>
2024-06-30 00:32:01 -0500

    Botón para volver al formulario
    Adición de botón para volver al formulario (no conserva los datos)

--- Lab8_Modo_Grafico/src/grafico/Reportes.java
+++ Lab8_Modo_Grafico/src/grafico/Reportes.java
@@ -2,22 +2,21 @@
 package grafico;
 
 //Importación de elementos de diseño
-import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-
+import java.awt.EventQueue;
+import java.awt.Font;
+import javax.swing.border.EmptyBorder;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.RowFilter;
-import javax.swing.JTable;
-import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableRowSorter;
 
@@ -28,17 +27,18 @@ import logica.Estudiantes;
 //Otras importaciones
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JButton;
 
 //Clase Reportes
 public class Reportes extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
-	//Elementos de interfaz (solo se crean para entradas de datos)
-	private JPanel contentPane;		//Contenedor de elementos
+	//Elementos dinámicos de interfaz
+	private JPanel contenedor;						//Contenedor de elementos
+	private JTable datosEstudiantes;
+	private DefaultTableModel  modeloTabla;
 	private JTextField textCedula;
 	private JComboBox<String> comboBoxCarreras;
-	private DefaultTableModel  modeloTabla;
-	private JTable datosEstudiantes;
 	private JComboBox<String> comboBoxSexo;
 	
 	//Ejecución de ventana
@@ -61,87 +61,91 @@ public class Reportes extends JFrame {
 		setBounds(100, 100, 666, 483);
 		
 		//CONFIGURACIÓN DEL CONTENEDOR
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contenedor = new JPanel();
+		contenedor.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contenedor);
+		contenedor.setLayout(null);
 		
 		//CONFIGURACIÓN DE ETIQUETAS
 		JLabel etiquetaReportes = new JLabel("Reportes");
 		etiquetaReportes.setBounds(238, 10, 149, 46);
 		etiquetaReportes.setFont(new Font("Tahoma", Font.PLAIN, 32));
-		contentPane.add(etiquetaReportes);
+		contenedor.add(etiquetaReportes);
 		
 		JLabel etiquetaEstudiantesBecados = new JLabel("Estudiantes Becados:");
 		etiquetaEstudiantesBecados.setBounds(52, 106, 221, 35);
 		etiquetaEstudiantesBecados.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		contentPane.add(etiquetaEstudiantesBecados);
+		contenedor.add(etiquetaEstudiantesBecados);
 		
 		JLabel etiquetaCedula = new JLabel("Cédula");
 		etiquetaCedula.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		etiquetaCedula.setBounds(52, 59, 51, 14);
-		contentPane.add(etiquetaCedula);
+		contenedor.add(etiquetaCedula);
 		
 		JLabel etiquetaCarrera = new JLabel("Carrera");
 		etiquetaCarrera.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		etiquetaCarrera.setBounds(274, 61, 49, 14);
-		contentPane.add(etiquetaCarrera);
+		contenedor.add(etiquetaCarrera);
+		
+		JLabel etiquetaSexo = new JLabel("Sexo");
+		etiquetaSexo.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		etiquetaSexo.setBounds(274, 86, 49, 22);
+		contenedor.add(etiquetaSexo);
 		
 		//CONFIGURACIÓN DE CAMPOS DE TEXTO
 		textCedula = new JTextField();
 		textCedula.setBounds(106, 58, 130, 20);
-		textCedula.addKeyListener(new KeyAdapter() {
-			@Override
+		textCedula.addKeyListener(new KeyAdapter() {				//Acción al ingresar una cédula
 			public void keyReleased(KeyEvent e) {
-				//Acción ejecutada al presionar el botón
 				filtro();
 			}
 		});
-		contentPane.add(textCedula);
-		
+		contenedor.add(textCedula);
 		
 		//CONFIGURACIÓN DE MENÚ DESPLEGABLE
 		comboBoxCarreras = new JComboBox<String>();
 		comboBoxCarreras.setModel(new DefaultComboBoxModel<String>(new String[] {"Todos","Ingeniería Civil", "Ingeniería Eléctrica", "Ingeniería Industrial", "Ingeniería en Sistemas", "Ingeniería Mecánica", "Ingeniería Marítima"}));
 		comboBoxCarreras.setBounds(328, 57, 184, 22);
-		comboBoxCarreras.addActionListener(new ActionListener() {
-			@Override
+		comboBoxCarreras.addActionListener(new ActionListener() {	//Acción al seleccionar una carrera
 			public void actionPerformed(ActionEvent e) {
-				//Acción ejecutada al presionar el botón
 				filtro();
 			}
 		});
+		contenedor.add(comboBoxCarreras);
 		
-		contentPane.add(comboBoxCarreras);
+		comboBoxSexo = new JComboBox<String>();
+		comboBoxSexo.setModel(new DefaultComboBoxModel<String>(new String[] {"Todos", "Masculino", "Femenino"}));
+		comboBoxSexo.setBounds(328, 90, 184, 20);
+		comboBoxSexo.addActionListener(new ActionListener() {		//Acción al seleccionar un sexo
+			public void actionPerformed(ActionEvent e) {
+				filtro();
+			}
+		});
+		contenedor.add(comboBoxSexo);
 		
 		//CONFIGURACIÓN DE TABLA
 		//Tabla
 		datosEstudiantes = new JTable();
-		datosEstudiantes.setBounds(24, 151, 603, 285);
-		contentPane.add(datosEstudiantes);
+		datosEstudiantes.setBounds(24, 151, 603, 254);
+		contenedor.add(datosEstudiantes);
 		
 		//Modelo de tabla
 		modeloTabla = new DefaultTableModel();
 		modeloTabla.setColumnIdentifiers(new String[] {"Nombre", "Cédula", "Carrera", "Índice", "Sexo"});
 		datosEstudiantes.setModel(modeloTabla);
 		
-		JLabel lblNewLabel = new JLabel("Sexo");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel.setBounds(274, 86, 49, 22);
-		contentPane.add(lblNewLabel);
-		
-		comboBoxSexo = new JComboBox();
-		comboBoxSexo.setModel(new DefaultComboBoxModel(new String[] {"Todos", "Masculino", "Femenino"}));
-		comboBoxSexo.setBounds(328, 90, 184, 20);
-		comboBoxSexo.addActionListener(new ActionListener() {
-			@Override
+		//CONFIGURACIÓN DE BOTONES
+		JButton btnFormulario = new JButton("Volver al Formulario");
+		btnFormulario.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnFormulario.setBounds(24, 415, 155, 21);
+		btnFormulario.addActionListener(new ActionListener() {			//Acción al presionar el botón
 			public void actionPerformed(ActionEvent e) {
-				//Acción ejecutada al presionar el botón
-				filtro();
+				dispose();
+				Formulario formulario = new Formulario();
+				formulario.setVisible(true);
 			}
 		});
-		contentPane.add(comboBoxSexo);
-		
+		contenedor.add(btnFormulario);
 		
 	} //Fin del constructor
 
@@ -155,7 +159,6 @@ public class Reportes extends JFrame {
         }
 
     }
-	
 	
 	void filtro() {
 		DefaultTableModel modelo = (DefaultTableModel) datosEstudiantes.getModel();
@@ -176,7 +179,7 @@ public class Reportes extends JFrame {
 
 	    String filtroSexo = (String) comboBoxSexo.getSelectedItem();
 	    if (!filtroSexo.equals("Todos")) {
-	        filtros.add(RowFilter.regexFilter(filtroSexo, 4)); // Índice 2 para la columna de sexo
+	        filtros.add(RowFilter.regexFilter(filtroSexo, 4)); // Índice 4 para la columna de sexo
 	    }
 	    
 	    RowFilter<Object, Object> filtroCompuesto = RowFilter.andFilter(filtros);
